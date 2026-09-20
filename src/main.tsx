dbb81fe4e7d5388a07f0f0e4dca1637d551ff395
Iveta Nováková <novaiv.ivet@gmail.com>
2026-09-20 14:41:24 +0200

    feat: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { LanguageProvider } from "./i18n/LanguageProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
       <App />
+      <Analytics />
     </LanguageProvider>
   </StrictMode>,
 );
