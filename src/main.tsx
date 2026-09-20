f70af2bd553d538d7e8064b5b96c74f3d37c8001
Iveta Nováková <novaiv.ivet@gmail.com>
2026-09-20 14:47:27 +0200

    fix: vercel analytics import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { LanguageProvider } from "./i18n/LanguageProvider";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
